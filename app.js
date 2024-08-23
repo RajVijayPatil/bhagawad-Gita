2d466bbd5e49de10cfbde6d9ab6dec1164cf1996
blackmage <rajvardhanpatil6400@gmail.com>
2024-08-23 16:40:51 +0530

    successfully retrieved information from the API.

--- app.js
+++ app.js
@@ -14,6 +14,25 @@ app.get('/', (req, res) => {
     res.render('index.ejs');
 });
 
+const config = {
+    headers: {
+        'x-rapidapi-key': 'e50a0cc3d1msh8af5f48ac4c1c67p19b9cbjsne83aeef2fc2e',
+        'x-rapidapi-host': 'bhagavad-gita3.p.rapidapi.com'
+    }
+};
+
+app.post('/getinfo', async (req, res) => {
+    const chapter = req.body.chapters;
+    try {
+        const result = await axios.get(`https://bhagavad-gita3.p.rapidapi.com/v2/chapters/${chapter}/`, config);
+        console.log(result.data);
+        res.render('content.ejs', { information: result.data });
+    } catch (error) {
+        console.error(error);
+        res.send('Error occured while fetching data.');
+    }
+});
+
 app.listen(PORT, () => {
     console.log('The server is running on port: ' + PORT);
 });
